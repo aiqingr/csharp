cdd51282607f1ec36c187b30465c31d6aa913f40
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-04 10:28:27 -0500

    example files added

---
+++ test1/test1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("I love you, yoyo.");
+        }
+    }
+}
---
+++ test2/test2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main1()
+        {
+            Console.WriteLine("I really love you, qiqi.");
+        }
+        static void Main()
+        {
+            Main1();
+            Console.WriteLine("I love you, ctq!");
+            Main1();
+        }
+    }
+}
