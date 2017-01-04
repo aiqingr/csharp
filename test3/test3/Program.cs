e6eddfde69e42357a61ec6acd237378aa10ecbdb
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-04 10:58:15 -0500

    input test

---
+++ test3/test3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter who you love:");
+
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Would you merry me, {0}?", Name);
+
+            Console.WriteLine("Would you merry me, " + Name + "?");
+
+            Console.WriteLine("Please enter your name:");
+
+            string Lastname = Console.ReadLine();
+
+            string Firstname = Console.ReadLine();
+
+            Console.WriteLine("Your name is {0} {1}", Lastname, Firstname);
+        }
+    }
+}
